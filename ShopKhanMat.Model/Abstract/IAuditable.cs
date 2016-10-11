488bc65cbf4fdfd9d3ec20ec83b7b721d2acea1f
PHU\Phu <dangphu.56pm@gmail.com>
2016-10-11 09:18:10 +0700

    Commit ShopKhanMat.Model

---
+++ ShopKhanMat.Model/Abstract/IAuditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ShopKhanMat.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdatedBy { get; set; }
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+        bool Status { get; set; }
+    }
+}
