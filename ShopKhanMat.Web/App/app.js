b28e6f1810a78cd8789d414ea0e53e1fc45dc2bf
PHU\Phu <dangphu.56pm@gmail.com>
2016-10-13 11:55:56 +0700

    route implement for app

--- ShopKhanMat.Web/App/app.js
+++ ShopKhanMat.Web/App/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿/// <reference path="F:\MyProject\Github\ShopKhanMat\ShopKhanMat.Web\Assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('shopKhanMat', ['shopKhanMat.products', 'shopKhanMat.common']).config(config);
+
+    config.$inject = ['$stateProvider','$urlRouterProvider']
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            Controller:"homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
---
+++ ShopKhanMat.Web/App/components/products/products.module.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    angular.module('shopKhanMat.products', ['shopKhanMat.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider']
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "/app/components/products/productListView.html",
+            Controller: "productListController"
+        }).state('product_add', {
+            url: "/product_add",
+            templateUrl: "/app/components/products/productAddView.html",
+            Controller: "productAddController"
+        });
+    }
+})();
