bc8500af8a6cfbf1875caa6e2dc0c147884f5006
PHU\Phu <dangphu.56pm@gmail.com>
2016-11-03 16:46:53 +0700

    pagination for products

--- ShopKhanMat.Web/Controllers/ProductController.cs
+++ ShopKhanMat.Web/Controllers/ProductController.cs
@@ -1,22 +1,48 @@
-﻿using System;
+﻿using AutoMapper;
+using ShopKhanMat.Common;
+using ShopKhanMat.Model.Models;
+using ShopKhanMat.Service;
+using ShopKhanMat.Web.InfaStructure.Core;
+using ShopKhanMat.Web.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ShopKhanMat.Web.Controllers
 {
     public class ProductController : Controller
     {
+        private IProductService _productService;
+        private IProductCategoryService _productCategoryService;
+
+        public ProductController(IProductService productService, IProductCategoryService productCategoryService)
+        {
+            this._productService = productService;
+            this._productCategoryService = productCategoryService;
+        }
+
         // GET: Product
         public ActionResult Detail(int productId)
         {
             return View();
         }
 
-        public ActionResult Category(int id)
+        public ActionResult Category(int id, int page = 1)
         {
-            return View();
+            int pageSize = int.Parse(ConfigHelper.GetByKey("PageSize"));
+            int totalRow = 0;
+            var productModel = _productService.GetListProductByCategoryIdPaging(id, page, pageSize, out totalRow);
+            var productViewModel = Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(productModel);
+            int totalPage = (int)Math.Ceiling((double)totalRow / pageSize);
+            var paginationSet = new PaginationSet<ProductViewModel>()
+            {
+                Items = productViewModel,
+                MaxPage = int.Parse(ConfigHelper.GetByKey("MaxPage")),
+                Page = page,
+                TotalCount = totalRow,
+                TotalPages = totalPage                                             
+            };
+            return View(paginationSet);
         }
     }
 }
