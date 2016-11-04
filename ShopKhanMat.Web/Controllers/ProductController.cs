f71babd08ec4ad4cef2c43e3df3de3b1da3e4ade
PHU\Phu <dangphu.56pm@gmail.com>
2016-11-04 09:16:45 +0700

    sorting produts

--- ShopKhanMat.Web/Controllers/ProductController.cs
+++ ShopKhanMat.Web/Controllers/ProductController.cs
@@ -27,13 +27,15 @@ namespace ShopKhanMat.Web.Controllers
             return View();
         }
 
-        public ActionResult Category(int id, int page = 1)
+        public ActionResult Category(int id, int page = 1, string sort = "")
         {
             int pageSize = int.Parse(ConfigHelper.GetByKey("PageSize"));
             int totalRow = 0;
-            var productModel = _productService.GetListProductByCategoryIdPaging(id, page, pageSize, out totalRow);
+            var productModel = _productService.GetListProductByCategoryIdPaging(id, page, pageSize, sort, out totalRow);
             var productViewModel = Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(productModel);
             int totalPage = (int)Math.Ceiling((double)totalRow / pageSize);
+            var category = _productCategoryService.GetById(id);
+            ViewBag.Category = Mapper.Map<ProductCategory, ProductCategoryViewModel>(category);
             var paginationSet = new PaginationSet<ProductViewModel>()
             {
                 Items = productViewModel,
